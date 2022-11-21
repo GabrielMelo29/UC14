f20a3d3af0d769c84ab620003a5756da887f6bf4
GabrielMelo29 <gbrlmelo29@gmail.com>
2022-11-21 20:33:30 -0300

    UC14

---
+++ Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,26 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+        }
+        public ChapterContext(DbContextOptions<ChapterContext> options) :base(options)
+        {
+        }
+
+        // configurar banco de dados
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = DESKTOP-0U0QDRJ\\SQLEXPRESS; initial Catalog = Chapter; Integrated Security = true");
+            }
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
